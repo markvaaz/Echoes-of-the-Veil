4602bb818bca1a0348e2f05fdc8fc0c8fb7e6b62
Mark Vaaz <marcosrodolfo32@gmail.com>
2025-08-29 19:18:10 -0300

    Add RequiredAlignment struct for alignment requirements handling

--- Components/Alignment.cs
+++ Components/Alignment.cs
@@ -4,14 +4,15 @@ public struct Alignment {
   public int Value;
 }
 
+public struct RequiredAlignment {
+  public int Value;
+  public AlignmentOperator Operator;
+}
+
 public enum AlignmentOperator {
   Equal,
   Greater,
   Less,
   GreaterOrEqual,
   LessOrEqual
-}
-public struct RequiredAlignment {
-  public int Value;
-  public AlignmentOperator Operator;
 }
